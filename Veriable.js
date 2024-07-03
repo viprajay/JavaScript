f19998fc8458aebc002865f285531d122d2bd045
viprajay dhali <73271252+viprajay@users.noreply.github.com>
2024-07-03 12:28:18 +0000

    make veriable types

--- Veriable.js
+++ Veriable.js
@@ -1,1 +1,17 @@
+
+
+const pin = 12345
+let myName = "Viprajay"
+var address = "Rudrapur"
+college = "Quantum"
+
+let phone
+
+// const pin = 22344 -> not possible
+let myname = "Viprajay Dhali"
+var address = "Quantum University"
+
+
 console.log("viprajay")
+console.log(myName)
+console.table([pin, myName, address,phone])
